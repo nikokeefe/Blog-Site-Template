80abee20ff9057dafe3d97aa36ba9a51b1a81c79
Nik O'Keefe <nikwillok@gmail.com>
2019-09-29 14:10:27 +1300

    Initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage = () => (
+	<>
+		<h1>About</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium iure
+			nobis cupiditate dolore in ipsam temporibus quis nesciunt atque magni.
+			Commodi quam pariatur numquam cupiditate deserunt itaque excepturi fugit
+			quasi.
+		</p>
+	</>
+);
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+	<>
+		<h1>My-Blog</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium iure
+			nobis cupiditate dolore in ipsam temporibus quis nesciunt atque magni.
+			Commodi quam pariatur numquam cupiditate deserunt itaque excepturi fugit
+			quasi.
+		</p>
+	</>
+);
+
+export default HomePage;
